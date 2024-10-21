f0c2fd9ba86991cbc98e11055b5fdcc092b4d31c
wnstj1548 <wnstj0614@naver.com>
2024-10-21 14:49:43 +0900

    mysql db연결 및 Configuration 작성 / application.yml 작성

---
+++ src/main/java/io/springbatch/springbatch/commons/config/DBJobConfiguration.java
@@ -0,0 +1,50 @@
+package io.springbatch.springbatch.commons.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class DBJobConfiguration {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step1 was executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info("step2 was executed");
+                    return RepeatStatus.FINISHED;
+                }), transactionManager)
+                .build();
+    }
+}
--- src/main/java/io/springbatch/springbatch/commons/config/HelloJobConfiguration.java
+++ src/main/java/io/springbatch/springbatch/commons/config/HelloJobConfiguration.java
@@ -1,4 +1,4 @@
-package io.springbatch.springbatch;
+package io.springbatch.springbatch.commons.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
